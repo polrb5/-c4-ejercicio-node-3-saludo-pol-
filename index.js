640f39bafaccb2bc1e3235ef8b5fd278e2b9612f
Pol Reig <polreigbroto@gmail.com>
2021-07-01 21:15:02 +0200

    Llamar función si se envia un parámetro y guardar saludo

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 require("dotenv").config();
+const { program } = require("commander");
+const { guardarSaludo } = require("./utilidades/archivos.js");
+const chalk = require("chalk");
+
+program.option("-n,--nombre <nombre>", "Tu nombre");
+program.parse();
+
+const opciones = program.opts();
+if (opciones.nombre) {
+  guardarSaludo(opciones.nombre);
+} else {
+  console.log(chalk.yellow("No se ha enviado el nombre"));
+  process.exit(1);
+}
