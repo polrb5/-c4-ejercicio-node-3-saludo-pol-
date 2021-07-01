2c4640d0b08dd32cdcf464a831bc072c67e80532
Pol Reig <polreigbroto@gmail.com>
2021-07-01 20:48:25 +0200

    Si hay error, mensaje en negrita y rojo

--- utilidades/archivos.js
+++ utilidades/archivos.js
@@ -1,4 +1,20 @@
 const fs = require("fs");
+const chalk = require("chalk");
+
+const error = chalk.bold.red;
 
 const guardarSaludo = (nombre) => {
-  fs.writeFile("textos/saludo.txt", `Hola, ${nombre}. Encantado de saludarte.`}
+  fs.writeFile(
+    "textos/saludo.txt",
+    `Hola, ${nombre}. Encantado de saludarte.`,
+    (err) => {
+      if (err) {
+        console.log(error("¡Algo ha salido mal!"));
+      }
+    }
+  );
+};
+
+module.exports = {
+  guardarSaludo,
+};
